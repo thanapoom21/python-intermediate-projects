620db1a8a913623120c7f46b53fd057293c987ef
thanapoom21 <thanapoom.p.music@gmail.com>
2021-08-14 16:36:11 -0700

    start and stop index with optional colon

--- lists.py
+++ lists.py
@@ -14,3 +14,13 @@ else:
 nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 print(nums[1:5])  # start index and stop index // [2, 3, 4, 5]
+
+print(nums[5::1]) # double colon means optional index
+
+print(nums[0::1]) # this will start from the first item the last item
+
+print(nums[::1])  # this will start from the first item the last item
+
+print(nums[::2])  # this will take every second items [1,3,5,7,9]
+
+print(nums[::-1])
