e0f61b98bac2b785fdc9f78fcef451d8ed21a508
thanapoom21 <thanapoom.p.music@gmail.com>
2021-08-18 23:40:08 -0700

    comparing the size of a list and a tuple

--- tuples.py
+++ tuples.py
@@ -1,3 +1,5 @@
+import sys
+
 # Tuple: ordered, immutable, allows duplicate elements
 mytuple = tuple(["Thanapoom", 32, "Las Vegas"])
 print(mytuple)
@@ -17,13 +19,20 @@ tu = (1, 2, 3, 4, 5, 6, 7, 8, 9)
 ple = tu[3:8]
 ple2 = tu[::-1]
 ple3 = tu[::2]
-print(ple)
-print(ple2)
-print(ple3)
+# print(ple)
+# print(ple2)
+# print(ple3)
 
 numbers_in_tuple = (2, 4, 6, 8, 10)
 # (*) with a variable will combine elements in between and put in a list.
 n1, *n2, n3 = numbers_in_tuple
-print(n1)
-print(n2)
-print(n3)
+# print(n1)
+# print(n2)
+# print(n3)
+
+# One advantage of using a tuple is its size is smaller compared to a list with the same elements.
+a_list = [0, 1, 2, 3, "google", True]
+a_tuple = (0, 1, 2, 3, "google", True)
+
+print(sys.getsizeof(a_list), "bytes")
+print(sys.getsizeof(a_tuple), "bytes")
