1a26b97db63ad006b5664cd7724162a52c7096eb
thanapoom21 <thanapoom.p.music@gmail.com>
2021-08-28 17:55:42 -0700

    experiment with copy(), variable assignment, dict()

--- dictionary.py
+++ dictionary.py
@@ -1,24 +1,30 @@
 # Dictionary: Key-value pairs, unordered, mutable.
 
 person = {"name": "Payet", "age": 30, "city": "Marseille"}
-print(person)
+# print(person)
 
-if "name" in person:
-    print(person["name"])
+# if "name" in person:
+#     print(person["name"])
 
-try:
-    print(person["age"])
-except:
-    print("Error")
+# try:
+#     print(person["age"])
+# except:
+#     print("Error")
 
-for key in person:
-    print(key)
+# for key in person:
+#     print(key)
 
-for key, value in person.items():
-    print(key, value)
+# for key, value in person.items():
+#     print(key, value)
 
 person_copy = person
 
+person_copy2 = person.copy()
+
+person_copy3 = dict(person)
+
 person_copy["email"] = "payetdimitri@gmail.com"
 print(person_copy)
+print(person_copy2)
+print(person_copy3)
 print(person)
